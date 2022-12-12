a667d5870a21a696856d3712f2a1a90c606cd534
Sebastien NINEL <42865820+shinseb5@users.noreply.github.com>
2022-12-12 23:04:25 +0100

    EXERCICES de list
    quelque exercice de "liste" python
    
    Signed-off-by: Sebastien NINEL <42865820+shinseb5@users.noreply.github.com>

---
+++ exercice_list.py
@@ -0,0 +1,36 @@
+# PENSEZ A DECOMMENTER POUR TESTER L'EXERCICE
+
+# TANT QUE i < 10 ALORS PASSE A LA BOUCLE SUIVANTE 
+
+# i = 0
+
+# while i < 10:
+# 	pass
+# 	i += 1
+	
+# resultat = "Exercice réussi !"
+# print(resultat)
+
+# ECRIRE LES INDEX DE "PYTHON" A LA VERTICALE
+
+# mot = "Python"
+
+# for i in range(len(mot)):
+#     print(i)
+
+
+# CREATION DE LA TABLE DE MULTIPLICATION DE 7
+
+# nombre = 7
+# i = 0
+
+# while i < 11:
+#     resultat = nombre * i
+#     print(f"{i} x {nombre} = {resultat}")
+#     i += 1
+
+# RECHERCHE DE L'INDEX ACCOMPAGNÉ DU PRENOM ASSOCIÉ
+
+# liste = ["Pierre", "Paul", "Marie"]
+# for i in liste:
+#     print(f"{liste.index(i)} {i}")
